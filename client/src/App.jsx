87153600f0c44b9fca25845292597bbcf6d0b131
ddanhs <ddanh000@citymail.cuny.edu>
2022-08-03 22:48:20 -0400

    add date picker

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,15 +2,24 @@ import React, { Fragment } from "react";
 import "./App.scss";
 import "bootstrap/dist/css/bootstrap.min.css";
 import axios from "axios";
-import { H1, Listbox, Field } from "@nyt-cms/ink";
+import { H1, Listbox, Field, Datepicker } from "@nyt-cms/ink";
 import * as pathOptions from "./directories/path.json";
+
+const current = new Date();
+const date = `${current.getDate()}/${
+  current.getMonth() + 1
+}/${current.getFullYear()}`;
 
 class App extends React.Component {
   state = {
     images: [],
     progress: 0,
-    directory: "NewsService",
+    directory: "",
+    // TODO: Set embargo to path???
     path: "undefined",
+    embargoDate: "",
+    currentDate: date,
+    renderSelector: "",
   };
   preventDefaults = (e) => {
     e.preventDefault();
@@ -22,7 +31,14 @@ class App extends React.Component {
     this.setState({
       progress: 0,
     });
-    formData.append("newKey", `${this.state.directory}/${this.state.path}/`);
+    if (this.state.directory == "Embargo") {
+      formData.append(
+        "newKey",
+        `${this.state.directory}/${this.state.embargoDate}/`
+      );
+    } else {
+      formData.append("newKey", `${this.state.directory}/${this.state.path}/`);
+    }
     files.forEach((file) => {
       formData.append("files", file, file.name);
     });
@@ -76,6 +92,7 @@ class App extends React.Component {
   handleDrop = (e) => {
     e.preventDefault();
     const files = e.dataTransfer.files;
+    // TODO: check everything is defined pls
     this.uploadFile(Array.from(files));
   };
 
@@ -84,19 +101,70 @@ class App extends React.Component {
   };
 
   handleInputByClick = (e) => {
+    // TODO: check everything is defined pls
     this.uploadFile(Array.from(e.target.files));
   };
 
   handleDrop1Change = (e) => {
     console.log("Handle Directory Dropdown Change e:");
     console.log(e);
-    this.setState({ directory: e });
+    if (e == "Embargo") {
+      this.setState({
+        directory: e,
+        renderSelector: this.renderDatepickerField(),
+      });
+    } else {
+      this.setState({ directory: e, renderSelector: this.renderPathField(e) });
+    }
   };
 
   handleDrop2Change = (e) => {
     console.log("Handle Path Dropdown Change e:");
     console.log(e);
     this.setState({ path: e });
+  };
+
+  handleDateChange = (e) => {
+    console.log("Handle Date Change e:");
+    console.log(e);
+    this.setState({ embargoDate: e });
+  };
+
+  renderPathField = (e) => {
+    return (
+      <Field id="listbox-label" label="Path">
+        <Listbox
+          id="listbox-control"
+          aria-labelledby="listbox-label"
+          onChange={this.handleDrop2Change}
+        >
+          {pathOptions[e].map((path) => (
+            <Listbox.Item value={path} />
+          ))}
+        </Listbox>
+      </Field>
+    );
+  };
+
+  //getMessage function - returns "" or error if date isn't correct
+
+  renderDatepickerField = () => {
+    return (
+      <Field
+        label="Select a date to release a file:"
+        // message={getMessage(this.embargoDate)}
+        // error={!!getMessage(this.embargoDate)}
+        id="dwv-field"
+      >
+        <Datepicker
+          // value={value}
+          // TODO: change to handleDrop2Change ?? set path to date
+          onChange={this.handleDateChange}
+          firstSelectableDate={this.currentDate}
+          id="dwv"
+        />
+      </Field>
+    );
   };
 
   componentDidMount() {
@@ -126,35 +194,25 @@ class App extends React.Component {
     return (
       <div className="App">
         <div className="container">
-          <H1>Drag and Drop Image Uploader with AWS S3</H1>
+          <H1>TNS: Sender</H1>
 
           <div className="dropDown">
             <Field id="listbox-label" label="Directory">
               <Listbox
-                id="listbox-control"
+                id="listbox-scontrol"
                 aria-labelledby="listbox-label"
-                defaultValue={"NewsService"}
                 onChange={this.handleDrop1Change}
               >
                 <Listbox.Item value="NewsService" />
                 <Listbox.Item value="IW_Send" />
-                <Listbox.Item value="TDRouter" />
+                <Listbox.Item value="Digest" />
                 <Listbox.Item value="Syndicate" />
                 <Listbox.Item value="Spending" />
+                <Listbox.Item value="Embargo" />
               </Listbox>
             </Field>
             <p className="mx-auto"></p>
-            <Field id="listbox-label" label="Path">
-              <Listbox
-                id="listbox-control"
-                aria-labelledby="listbox-label"
-                onChange={this.handleDrop2Change}
-              >
-                {pathOptions[this.state.directory].map((path) => (
-                  <Listbox.Item value={path} />
-                ))}
-              </Listbox>
-            </Field>
+            {this.state.renderSelector}
           </div>
 
           <div
